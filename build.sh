a3b657bc68c94594a7de4659190d796684ee5717
zimlit <zimlit2010@gmail.com>
2023-05-20 16:48:57 -0400

    'mourning never comes' and 'whispers in the dark' flowchar

---
+++ build.sh
@@ -0,0 +1,19 @@
+# Copyright (C) 2023 Devin Rockwell
+# 
+# This file is part of when silence dies.
+# 
+# when silence dies is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# when silence dies is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with when silence dies.  If not, see <http://www.gnu.org/licenses/>.
+
+dot planning.dot -Tpng -o build/planning.png
+dot planning.dot -Tsvg -o build/planning.svg
